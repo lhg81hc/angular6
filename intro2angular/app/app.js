e414fbb184a554e0669db1df3631cb5529bbdf37
thangle-Antenor <thangle-antenor@thangle.local>
2024-04-29 22:24:20 +0700

    Practice Filters

--- intro2angular/app/app.js
+++ intro2angular/app/app.js
@@ -2,5 +2,21 @@ let myApp = angular.module('myApp', []);
 
 myApp.controller('IndexController', ['$scope', function($scope) {
   $scope.message = "This is the message from $scope";
-  $scope.options = ['pork', 'chicken', 'beef', 'lamb'];
+  $scope.options = [
+    {
+      first_name: 'Thad',
+      last_name: 'Le',
+      age: 33
+    },
+    {
+      first_name: 'Jason',
+      last_name: 'Bourne',
+      age: 35,
+    },
+    {
+      first_name: 'David',
+      last_name: 'Webb',
+      age: null,
+    }
+  ];
 }]);
