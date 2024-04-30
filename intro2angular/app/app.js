9ca27ce41bf2ecfc819a23b795b62fb6fed2d7f1
thangle-Antenor <thangle-antenor@thangle.local>
2024-04-30 11:37:24 +0700

    Practice ng-click

--- intro2angular/app/app.js
+++ intro2angular/app/app.js
@@ -2,38 +2,43 @@ let myApp = angular.module('myApp', []);
 
 myApp.controller('IndexController', ['$scope', function($scope) {
   $scope.message = "This is the message from $scope";
-  $scope.options = [
+  $scope.characters = [
     {
+      id: 233,
       first_name: 'Thad',
       last_name: 'Le',
       age: 33,
       available: true,
-      gender: 'Male',
-      gender_abbreviation: 'M',
+      gender: 'm',
     },
     {
+      id: 644,
       first_name: 'Jason',
       last_name: 'Bourne',
       age: 30,
       available: true,
-      gender: 'Male',
-      gender_abbreviation: 'M',
+      gender: 'm',
     },
     {
+      id: 113,
       first_name: 'Ethan',
       last_name: 'Hunt',
       age: 32,
       available: false,
-      gender: 'Male',
-      gender_abbreviation: 'M',
+      gender: 'm',
     },
     {
+      id: 10,
       first_name: 'Evelyn',
       last_name: 'Salt',
       age: 25,
       available: true,
-      gender: 'Female',
-      gender_abbreviation: 'F'
+      gender: 'f',
     }
   ];
+
+  $scope.removeCharacter = function(character) {
+    let removedCharacter = $scope.characters.indexOf(character);
+    $scope.characters.splice(removedCharacter, 1);
+  };
 }]);
