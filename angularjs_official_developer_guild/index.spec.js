9800b679ed8790e5af4109c700190560aa4989bc
Thad Le <llengocthangg@gmail.com>
2024-05-31 11:06:11 +0700

    Try test spec for index.html

---
+++ angularjs_official_developer_guild/index.spec.js
@@ -0,0 +1,18 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+
+describe('PhoneListController', function() {
+  beforeEach(module('phonecatApp'));
+
+  it('should create a `phones` model with 3 phones', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', { $scope: scope });
+
+    expect(scope.phones.length).toBe(3);
+  }));
+});
---
+++ angularjs_official_developer_guild/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  clearMocks: true,
+  setupFilesAfterEnv: ['regenerator-runtime/runtime'],
+  roots: [
+    "<rootDir>",
+  ],
+  modulePaths: [
+    "<rootDir>",
+  ],
+  moduleDirectories: ["node_modules"],
+};
